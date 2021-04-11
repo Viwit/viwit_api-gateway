213acd2076fba8ff76d99c9a1b19c715a129fd83
daviidleon <davidleonmz@gmail.com>
2021-04-10 23:39:22 -0500

    imports for resolvers-schemas

--- graphql/general/resolvers.js
+++ graphql/general/resolvers.js
@@ -6,9 +6,13 @@ const GENERAL_URL = 'http://localhost:4000';
 //API TOOL USING AXIOS FOR SENDING REQUESTS
 const api = apiAdapter(GENERAL_URL);
 
-async function getBusByLicensePlate({licensePlate}){
-  res = await api.get(`/Bus/${licensePlate}`)
-  return res.data  
+module.exports = {
+  getBusByLicensePlate : async function({licensePlate}){
+    res = await api.get(`/Bus/${licensePlate}`)
+    return res.data  
+  }
 }
+
+
 
 
