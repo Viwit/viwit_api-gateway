cd0e1d722b62fb603638216604f98183726855b9
dleonmu <dleonmu@unal.edu.co>
2021-04-15 00:49:46 -0500

    minor fixes

--- graphql/schemas.js
+++ graphql/schemas.js
@@ -1,18 +1,19 @@
 const {buildSchema} = require('graphql');
 const mergeSchemas = require('../util/mergeSchemas')
+
+//IMPORT SCHEMAS FROM EACH MICROSERVICE
 const {scoreTypeDef, scoreMutations, scoreQueries} = require('./score/schema');
 const {generalTypeDef, generalMutations, generalQueries} = require('./general/schema')
 const {transactionTypeDef, transactionMutations, transactionQueries} = require('./transaction/schema');
+const {authenticationTypeDef, authenticationMutations, authenticationQueries} = require('./auth1/schema');
+const {tokenTypeDef, tokenQueries, tokenMutations} = require('./auth2/schema');
 
-const {authenticationTypeDef, authenticationMutations, authenticationQueries} = require('./authentication1/schema');
+//MERGE IN ARRAYS
 let typeDefs = [scoreTypeDef, generalTypeDef,transactionTypeDef,authenticationTypeDef ]
 let queries = [scoreQueries, generalQueries,transactionQueries,authenticationQueries ]
 let mutations = [scoreMutations, generalMutations, transactionMutations,authenticationMutations]
 
-const {tokenTypeDef, tokenQueries, tokenMutations} = require('./Authentication2/schema')
-
-
+//CONCATENATE AND BUILD MAIN SCHEMA
 const mainSchema = buildSchema(mergeSchemas(typeDefs,queries,mutations));
 
 module.exports = mainSchema
-
