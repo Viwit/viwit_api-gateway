cd1d4bb39856d37da79447cfcb1da56858c7aac9
dleonmu <dleonmu@unal.edu.co>
2021-05-01 02:10:41 -0500

    cors-policy-enable

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
+var cors = require('cors');
+
 
 const graphqlSchema = require('./graphql/schemas');
 const graphqlResolvers = require('./graphql/resolvers');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(
