f4d2ed04253d73091a5eafc261d08b946d78f3a6
Edisson <edprietov@unal.edu.co>
2021-05-02 08:15:54 -0500

    Wallet creation with user sign up

--- graphql/auth1/resolvers.js
+++ graphql/auth1/resolvers.js
@@ -1,35 +1,58 @@
 const apiAdapter = require('../../util/apiAdapter')
+const {
+    Transaction,
+    postCreateWallet
+} = require('../../graphql/transaction/resolvers');
 
 //BASE URL FOR THE MICROSERVICE
-const {AUTHENTICATION_URL} = require('../../util/servers') 
+const {
+    AUTHENTICATION_URL
+} = require('../../util/servers')
 
 //API TOOL USING AXIOS FOR SENDING REQUESTS
 const api = apiAdapter(AUTHENTICATION_URL)
 
 module.exports = {
-  getuser: async function ({id}) {
-    res = await api.get(`/user/${id}`)
-    return res.data
-  },
+    getuser: async function({
+        id
+    }) {
+        res = await api.get(`/user/${id}`)
+        return res.data
+    },
 
-  getusers: async function () {
-    res = await api.get(`/users`)
-    return res.data
-  },
+    getusers: async function() {
+        res = await api.get(`/users`)
+        return res.data
+    },
 
-  setUser: async function ({user}) {
-    res = await api.post(`/user`, user)
-    return res.data
-  },
+    setUser: async function({
+        user
+    }) {
+        res = await api.post(`/user`, user)
+        if (res.data != null) {
+            postCreateWallet({
+                wallet: {
+                    token: "generico", //Cambiar al token que se genera para cada usuario
+                    user_id: 1 //Cambiar al id del usuario
+                }
+            })
+        }
+        return res.data
+    },
 
-  updateUser: async function ({id,user}){
-    res = await api.put(`/user/${id}`,user)
-    return res.data
-  },
+    updateUser: async function({
+        id,
+        user
+    }) {
+        res = await api.put(`/user/${id}`, user)
+        return res.data
+    },
 
-  deleteUser: async function ({id}){
-    res = await api.delete(`/user/${id}`)
-    res.data.success=true
-    return res.data
-  }
+    deleteUser: async function({
+        id
+    }) {
+        res = await api.delete(`/user/${id}`)
+        res.data.success = true
+        return res.data
+    }
 }
