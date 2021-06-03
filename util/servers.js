8d6e3f46bc16e594c6182599d210b663cd04f444
dleonmu <dleonmu@unal.edu.co>
2021-06-03 17:46:59 -0500

    url-general

--- util/servers.js
+++ util/servers.js
@@ -1,7 +1,8 @@
 const AUTHENTICATION_URL = 'http://3.214.252.90:3000/api';
 const AUTHENTICATION_2_URL = 'http://54.224.78.129:8080';
 const TRANSACTION_URL = 'http://ec2-18-212-92-146.compute-1.amazonaws.com:4100';
-const GENERAL_URL = 'http://ec2-18-206-64-117.compute-1.amazonaws.com:4000';
+const GENERAL_URL = 'http://ec2-18-212-198-246.compute-1.amazonaws.com:4000';
+
 const SCORE_URL = 'http://172.17.0.1:8000';
 const QR_URL = 'http://ec2-34-229-49-90.compute-1.amazonaws.com:8050';
 
