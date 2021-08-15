2ceccf6eb86b9cdd0b5688d34ea106e9bb9ead72
Edprietov <edprietov@unal.edu.co>
2021-08-14 19:20:42 -0500

    Actualización de IPs.

--- util/servers.js
+++ util/servers.js
@@ -1,10 +1,10 @@
-const AUTHENTICATION_URL = 'http://3.214.252.90:3000/api';
-const AUTHENTICATION_2_URL = 'http://54.224.78.129:8080';
-const TRANSACTION_URL = 'http://ec2-18-212-92-146.compute-1.amazonaws.com:4100';
+const AUTHENTICATION_URL = 'http://100.25.223.26:3000/api';
+const AUTHENTICATION_2_URL = 'http://100.25.223.26:8080';
+const TRANSACTION_URL = 'http://100.25.223.26:4100';
 const GENERAL_URL = 'http://ec2-54-91-189-146.compute-1.amazonaws.com:4000';
 
 const SCORE_URL = 'http://172.17.0.1:8000';
-const QR_URL = 'http://ec2-34-229-49-90.compute-1.amazonaws.com:8050';
+const QR_URL = 'http://100.25.223.26:8050';
 
 module.exports = {
   AUTHENTICATION_URL,
