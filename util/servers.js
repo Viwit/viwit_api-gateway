8faa80480f691e77be5b2d90c9b49bd1fa5afed9
Edprietov <edprietov@unal.edu.co>
2021-08-17 12:44:34 -0500

    IPs updated

--- util/servers.js
+++ util/servers.js
@@ -1,6 +1,6 @@
-const AUTHENTICATION_URL = 'http://100.25.223.26:3000/api';
-const AUTHENTICATION_2_URL = 'http://100.25.223.26:8080';
-const TRANSACTION_URL = 'http://100.25.223.26:4100';
+const AUTHENTICATION_URL = 'http://54.237.94.101:3000/api';
+const AUTHENTICATION_2_URL = 'http://54.237.94.101:8080';
+const TRANSACTION_URL = 'http://54.237.94.101:4100';
 const GENERAL_URL = 'http://ec2-54-91-189-146.compute-1.amazonaws.com:4000';
 
 const SCORE_URL = 'http://172.17.0.1:8000';
