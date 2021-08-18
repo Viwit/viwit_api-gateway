c2af2b791e5227c76b827bc7582a9c60a259ff17
dleonmu <79862993+dleonmu@users.noreply.github.com>
2021-08-18 15:51:31 -0500

    url change general 1912712

--- util/servers.js
+++ util/servers.js
@@ -1,7 +1,7 @@
 const AUTHENTICATION_URL = 'http://54.237.94.101:3000/api';
 const AUTHENTICATION_2_URL = 'http://54.237.94.101:8080';
 const TRANSACTION_URL = 'http://54.237.94.101:4100';
-const GENERAL_URL = 'http://ec2-54-91-189-146.compute-1.amazonaws.com:4000';
+const GENERAL_URL = 'http://ec2-3-239-247-115.compute-1.amazonaws.com:4000';
 
 const SCORE_URL = 'http://172.17.0.1:8000';
 const QR_URL = 'http://100.25.223.26:8050';
